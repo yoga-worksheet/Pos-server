b9821b82d724749f3a7e8b5f114563f2075f5ea4
Yoga Worksheet <yoga.worksheet@gmail.com>
2022-10-21 09:49:14 +0700

    edit config for mongodb atlas

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { dbHost, dbPort, dbUser, dbPass, dbName } = require("../app/config");
-const URI = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
+// const URI = `mongodb+srv://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
+const URI = `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/?retryWrites=true&w=majority`;
 
 mongoose.connect(URI);
 const db = mongoose.connection;
